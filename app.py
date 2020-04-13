ffed8ccc61f90d0d331387cc32cd834517b3d689
Matt-Donovan <matt.donovan@convoymedia.com>
2020-04-13 02:44:34 +0100

    base commit

--- app.py
+++ app.py
@@ -1,14 +1,34 @@
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Resource, Api
 
 
 app = Flask(__name__)
+api = Api(app)
+
+items = []
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+class Item(Resource):
+    def get(self, name):
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {'item': item}, 200 if item else 404
+
+    def post(self, name):
+        if next(filter(lambda x: x['name'] == name, items), None):
+            return {'message': "An item with name '{}' already exists.".format(name)}, 400
+
+        data = request.get_json()
+        item = {'name': name, 'price': data['price']}
+        items.append(item)
+        return item, 201
+
+class ItemsList(Resource):
+    def get(self):
+        return {'items': items}
 
 
 if __name__ == '__main__':
-    app.run()
+    api.add_resource(Item, '/item/<string:name>')
+    api.add_resource(ItemsList, '/items')
+    app.run(port=5000, debug=True)
