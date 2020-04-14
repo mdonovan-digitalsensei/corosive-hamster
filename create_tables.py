057eda042d934a09f95596a317796dba64a03956
Matt-Donovan <matt.donovan@convoymedia.com>
2020-04-14 16:53:33 +0100

    added item class

--- create_tables.py
+++ create_tables.py
@@ -4,6 +4,7 @@ connection = sqlite3.connect('data.db')
 cursor = connection.cursor()
 
 create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
+create_table = "CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY, item text, price text)"
 cursor.execute(create_table)
 
 connection.commit()
