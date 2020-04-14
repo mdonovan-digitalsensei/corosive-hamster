1ea9751801d6013e7d4a49492ec9c2f56b9f26fd
Matt-Donovan <matt.donovan@convoymedia.com>
2020-04-14 17:16:41 +0100

    updated put command to allow insert into the database and moved add_resource options to outside of the if __name__ block.

--- app.py
+++ app.py
@@ -12,8 +12,9 @@ api = Api(app)
 
 jwt = JWT(app, authenticate, identity)
 
+api.add_resource(Item, "/item/<string:name>")
+api.add_resource(ItemsList, "/items")
+api.add_resource(UserRegister, "/register")
+
 if __name__ == "__main__":
-    api.add_resource(Item, "/item/<string:name>")
-    api.add_resource(ItemsList, "/items")
-    api.add_resource(UserRegister, "/register")
     app.run(port=5000, debug=True)
--- item.py
+++ item.py
@@ -2,14 +2,15 @@ import sqlite3
 from flask_jwt import jwt_required
 from flask_restful import Resource, reqparse
 
+
 class Item(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument(
         "price", type=float, required=True, help="This field cannot be left blank!"
     )
 
-    @jwt_required()
-    def get(self, name):
+    @classmethod
+    def find_by_name(cls, name):
         connection = sqlite3.connect("data.db")
         cursor = connection.cursor()
         query = "SELECT * FROM items WHERE name = ?"
@@ -17,11 +18,17 @@ class Item(Resource):
         row = result.fetchone()
         connection.close()
         if row:
-            return {'item': {'name': row[1], 'price': row[2]}}
-        return {'message': 'Item not found'}, 404
+            return {"item": {"name": row[1], "price": row[2]}}
+
+    @jwt_required()
+    def get(self, name):
+        item = self.find_by_name(name)
+        if item:
+            return item
+        return {"message": "Item not found"}, 404
 
     def post(self, name):
-        if next(filter(lambda x: x["name"] == name, items), None):
+        if self.find_by_name(name):
             return (
                 {"message": "An item with name '{}' already exists.".format(name)},
                 400,
@@ -29,7 +36,13 @@ class Item(Resource):
 
         data = Item.parser.parse_args()
         item = {"name": name, "price": data["price"]}
-        items.append(item)
+
+        connection = sqlite3.connect("data.db")
+        cursor = connection.cursor()
+        query = "INSERT INTO items VALUES (NULL, ?, ?)"
+        cursor.execute(query, (item["name"], item["price"]))
+        connection.commit()
+        connection.close()
         return item, 201
 
     def delete(self, name):
@@ -52,6 +65,5 @@ class Item(Resource):
 
 class ItemsList(Resource):
     def get(self):
-
 
         return {"items": items}
