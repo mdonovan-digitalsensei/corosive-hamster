95604856b3ec7f7580e8c75e9747b19bb724e82d
Matt-Donovan <matt.donovan@convoymedia.com>
2020-04-14 16:07:45 +0100

    revised with black

--- user.py
+++ user.py
@@ -1,6 +1,7 @@
 import sqlite3
 from flask_restful import Resource, reqparse
 
+
 class User:
     def __init__(self, _id, username, password):
         self.id = _id
@@ -9,7 +10,7 @@ class User:
 
     @classmethod
     def find_by_username(cls, username):
-        connection = sqlite3.connect('data.db')
+        connection = sqlite3.connect("data.db")
         cursor = connection.cursor()
 
         query = "SELECT * FROM users WHERE username=?"
@@ -24,7 +25,7 @@ class User:
 
     @classmethod
     def find_by_id(cls, _id):
-        connection = sqlite3.connect('data.db')
+        connection = sqlite3.connect("data.db")
         cursor = connection.cursor()
 
         query = "SELECT * FROM users WHERE id=?"
@@ -37,29 +38,27 @@ class User:
         connection.close()
         return user
 
+
 class UserRegister(Resource):
     parser = reqparse.RequestParser()
-    parser.add_argument('username',
-                        type=str,
-                        required=True,
-                        help="Username field cannot be left blank!"
-                        )
-    parser.add_argument('password',
-                        type=str,
-                        required=True,
-                        help="Password field cannot be left blank!")
+    parser.add_argument(
+        "username", type=str, required=True, help="Username field cannot be left blank!"
+    )
+    parser.add_argument(
+        "password", type=str, required=True, help="Password field cannot be left blank!"
+    )
 
     def post(self):
         data = UserRegister.parser.parse_args()
 
-        if User.find_by_username(data['username']):
+        if User.find_by_username(data["username"]):
             return {"message": "User already exists"}, 400
 
-        connection = sqlite3.connect('data.db')
+        connection = sqlite3.connect("data.db")
         cursor = connection.cursor()
 
         query = "INSERT INTO users VALUES (NULL, ?, ?)"
-        cursor.execute(query, (data['username'],data['password']))
+        cursor.execute(query, (data["username"], data["password"]))
 
         connection.commit()
         connection.close()
