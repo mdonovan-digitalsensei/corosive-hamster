faa0f68f312e3c57cdf9684b39f9fa8c5831363a
Matt-Donovan <matt.donovan@convoymedia.com>
2020-04-14 16:08:03 +0100

    revised with black

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from security import authenticate, identity
 from user import UserRegister
 
 app = Flask(__name__)
-app.secret_key = 'dave'
+app.secret_key = "dave"
 api = Api(app)
 
 jwt = JWT(app, authenticate, identity)
@@ -17,50 +17,52 @@ items = []
 
 class Item(Resource):
     parser = reqparse.RequestParser()
-    parser.add_argument('price',
-                        type=float,
-                        required=True,
-                        help="This field cannot be left blank!"
-                        )
+    parser.add_argument(
+        "price", type=float, required=True, help="This field cannot be left blank!"
+    )
 
     @jwt_required()
     def get(self, name):
-        item = next(filter(lambda x: x['name'] == name, items), None)
-        return {'item': item}, 200 if item else 404
+        item = next(filter(lambda x: x["name"] == name, items), None)
+        return {"item": item}, 200 if item else 404
 
     def post(self, name):
-        if next(filter(lambda x: x['name'] == name, items), None):
-            return {'message': "An item with name '{}' already exists.".format(name)}, 400
+        if next(filter(lambda x: x["name"] == name, items), None):
+            return (
+                {"message": "An item with name '{}' already exists.".format(name)},
+                400,
+            )
 
         data = Item.parser.parse_args()
-        item = {'name': name, 'price': data['price']}
+        item = {"name": name, "price": data["price"]}
         items.append(item)
         return item, 201
 
     def delete(self, name):
         global items
-        items = list(filter(lambda x: x['name'] != name, items))
-        return {'message': 'Item deleted'}
+        items = list(filter(lambda x: x["name"] != name, items))
+        return {"message": "Item deleted"}
 
     def put(self, name):
 
         data = Item.parser.parse_args()
 
-        item = next(filter(lambda x: x['name'] == name, items), None)
+        item = next(filter(lambda x: x["name"] == name, items), None)
         if item is None:
-            item = {'name': name, 'price': data['price']}
+            item = {"name": name, "price": data["price"]}
             items.append(item)
         else:
             item.update(data)
         return item
 
+
 class ItemsList(Resource):
     def get(self):
-        return {'items': items}
+        return {"items": items}
 
 
-if __name__ == '__main__':
-    api.add_resource(Item, '/item/<string:name>')
-    api.add_resource(ItemsList, '/items')
-    api.add_resource(UserRegister, '/register')
+if __name__ == "__main__":
+    api.add_resource(Item, "/item/<string:name>")
+    api.add_resource(ItemsList, "/items")
+    api.add_resource(UserRegister, "/register")
     app.run(port=5000, debug=True)
