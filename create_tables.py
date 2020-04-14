e3a73a337d4a2ca093d9d549ba3cd96c5f13684b
Matt-Donovan <matt.donovan@convoymedia.com>
2020-04-14 16:57:00 +0100

    fixed create tables

--- create_tables.py
+++ create_tables.py
@@ -3,9 +3,10 @@ import sqlite3
 connection = sqlite3.connect('data.db')
 cursor = connection.cursor()
 
-create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
-create_table = "CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY, item text, price text)"
-cursor.execute(create_table)
+create_table0 = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
+create_table1 = "CREATE TABLE IF NOT EXISTS items (id INTEGER PRIMARY KEY, item text, price text)"
+cursor.execute(create_table0)
+cursor.execute(create_table1)
 
 connection.commit()
 connection.close()
