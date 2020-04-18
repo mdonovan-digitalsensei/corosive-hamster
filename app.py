aedf7cac04bbb7b85291860c7e7551dbcf1a9d26
Matt-Donovan <mdonovan@digitalsensei.uk>
2020-04-18 01:34:27 +0100

    implemented delete for users

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from flask_jwt import JWT
 
 from resource.item import Item, ItemsList
 from security import authenticate, identity
-from resource.user import UserRegister
+from resource.user import UserRegister, User
+from resource.store import Store, StoreList
 from db import db
 
 
@@ -14,11 +15,20 @@ app.config['SQLALCHEMY_TRACK_MODIFCATIONS'] = False
 app.secret_key = "dave"
 api = Api(app)
 
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
 jwt = JWT(app, authenticate, identity)
 
 api.add_resource(Item, "/item/<string:name>")
 api.add_resource(ItemsList, "/items")
 api.add_resource(UserRegister, "/register")
+api.add_resource(Store, "/store/<string:name>")
+api.add_resource(StoreList, "/stores")
+api.add.resource(User, '/user/<int:user_id>')
+
 
 if __name__ == "__main__":
     db.init_app(app)
--- resource/item.py
+++ resource/item.py
@@ -9,6 +9,9 @@ class Item(Resource):
     parser.add_argument(
         "price", type=float, required=True, help="This field cannot be left blank!"
     )
+    parser.add_argument(
+        "store_id", type=int, required=True, help="This field cannot be left blank!"
+    )
 
     @jwt_required()
     def get(self, name):
@@ -25,7 +28,7 @@ class Item(Resource):
             )
 
         data = Item.parser.parse_args()
-        item = ItemModel(name, data["price"])
+        item = ItemModel(name, data["price"], data["store_id"])
         # try:
         item.save_to_db()
         # except:
@@ -45,7 +48,7 @@ class Item(Resource):
         item = ItemModel.find_by_name(name)
 
         if item is None:
-            item = ItemModel(name, data['price'])
+            item = ItemModel(name, data['price'], data["store_id"])
         else:
             item.price = data['price']
 
